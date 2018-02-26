3779be191abdcd0917bdd2dbcebaf10d275d7227
Ray <rtitusca@yahoo.ca>
2018-02-26 18:27:45 -0500

    initial commit

---
+++ customerreviewserver.jar.src/de/hybris/platform/customerreview/setup/CurseWords.java
@@ -0,0 +1,17 @@
+package de.hybris.platform.customerreview.setup;
+
+import java.util.List;
+
+public class CurseWords {
+    private List<String> curseWords;
+
+    public CurseWords(List<String> curseWords) {
+        this.curseWords = curseWords;
+    }
+
+    public List<String> getCurseWords() {
+        return curseWords;
+    }
+
+
+}
